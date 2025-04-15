45a3e9a7fce94235715e2a690a53c4cd95eabff9
Jonas Beck <jonasb@outlook.dk>
2025-04-15 12:25:21 +0200

    feat: Day 07 - Part A
    Signed-off-by: Jonas Beck <jonasb@outlook.dk>

---
+++ 2024/golang/src/day07/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	day07a := day07a("day07a.txt")
+	fmt.Printf("Day 07 Part A: %d\n", day07a)
+	day07b := day07b("day07b.txt")
+	fmt.Printf("Day 06 Part B: %d\n", day07b)
+}
+
+func readFile(filename string) map[int][]int {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	// Initialize scanner
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	numbers := make(map[int][]int)
+
+	// Scan each line in file
+	for scanner.Scan() {
+		split := strings.Split(scanner.Text(), ":")
+
+		key, _ := strconv.Atoi(split[0])
+
+		numbers[key] = readInts(split[1])
+
+	}
+
+	return numbers
+}
+
+func readInts(line string) []int {
+	ints := make([]int, 0)
+
+	for _, value := range strings.Fields(line) {
+		int, _ := strconv.Atoi(value)
+		ints = append(ints, int)
+	}
+
+	return ints
+}
+
+func calculateEquation(target int, numbers []int, operators []rune) int {
+	var ints []int
+
+	ints = getOperatorResults(target, numbers[1], []int{numbers[0]}, operators)
+
+	for _, number := range numbers[2:] {
+		ints = getOperatorResults(target, number, ints, operators)
+	}
+
+	if present := slices.Contains(ints, target); present {
+		return target
+	}
+
+	return 0
+}
+
+func getOperatorResults(target, number int, numbers []int, operators []rune) []int {
+	result := make([]int, 0)
+
+	for _, value := range numbers {
+		for _, operator := range operators {
+			operatorResult := applyOperator(value, number, operator)
+
+			// Filter out numbers that are to big
+			if operatorResult <= target {
+				result = append(result, operatorResult)
+			}
+		}
+	}
+
+	return result
+}
+
+func applyOperator(a, b int, op rune) int {
+	switch op {
+	case '+':
+		return a + b
+	case '*':
+		return a * b
+	default:
+		return 0 // Default case, though with your operators this wouldn't be reached
+	}
+}
+
+func day07a(filename string) int {
+	numbers := readFile(filename)
+
+	result := 0
+
+	operators := []rune{'+', '*'}
+
+	for target, values := range numbers {
+		fmt.Println(target, values)
+		result += calculateEquation(target, values, operators)
+	}
+
+	return result
+}
+
+func day07b(filename string) int {
+	// guardMap, startPosition := readFile(filename)
+
+	return 0
+}
